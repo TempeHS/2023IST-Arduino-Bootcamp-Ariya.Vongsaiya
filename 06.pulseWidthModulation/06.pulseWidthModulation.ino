1b70674d1a8cfe410b6f3f7bafe50b9d350ca249
Ariya02 <ariya.vongsaiya209@education.nsw.gov.au>
2023-08-09 10:33:57 +1000

    06 pulseWidthModulation completed

--- 06.pulseWidthModulation/06.pulseWidthModulation.ino
+++ 06.pulseWidthModulation/06.pulseWidthModulation.ino
@@ -1,5 +1,5 @@
 /*
-  Author: 
+  Author: Ariya Vongsaiya
   Learning Intention: The students will learn what 'pulse width modulation' is and how to use it to write analogue data on a digital PIN.
   Success Criteria:
     1. I understand what pulse width modulation is
@@ -8,18 +8,31 @@
     5. I can write PWM data to multiple pins and manually change the colour of the LED
 
   Student Notes: 
-
+  Follow steps to limit mistakes:
+  1. Configure serial monitor
+  2. Debug Serial monitor
+  3. check if it prints onto serial monitor
+  Value range for PWM 0-255
   Documentation: 
     https://www.w3schools.com/colors/colors_rgb.aspt
   Schematic:
-    https://www.tinkercad.com/things/drNDsMxGMIY?sharecode=D6x4_xTTRl_zGs7B-XGqfbQhG_xgjj3TVS_c-GNTK-c
+    https://www.tinkcad.com/things/drNDsMxGMIY?sharecode=D6x4_xTTRl_zGs7B-XGqfbQhG_xgjj3TVS_c-GNTK-c
     https://github.com/TempeHS/TempeHS_Ardunio_Boilerplate/blob/main/Ardunio_Bootcamp/06.pulseWidthModulation/Bootcamp-PWMOutput.png
 */
+unsigned static int lightSensor = A0; 
+unsigned static int myLED = 5;
 
 void setup() {
-
+Serial.begin(9600); 
+Serial.println("Serial monitor configured to 9600");
+Serial.println("--------------------------------");
+pinMode(myLED, OUTPUT);
 }
 
 void loop() {
-
+int val = analogRead(lightSensor);
+Serial.print("LightSensor:");
+Serial.println(val);
+int LEDbright = val/4; 
+analogWrite(myLED, LEDbright);
 }
